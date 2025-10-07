05baa949569369ec8f00fbc246f4af816f97ec1b
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2025-10-06 23:32:50 -0300

    [lang]: add pt-br as main lang & en as fallback

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,22 +1,20 @@
-import { createI18n } from 'vue-i18n'
+import { createI18n } from "vue-i18n";
 
-import en from '../locales/en.json'
-// Add more languages as you create them
-// import es from '../locales/es.json'
-// import fr from '../locales/fr.json'
+import en from "../locales/en.json";
+import pt from "../locales/pt.json";
 
 const messages = {
   en,
-  // es,
+  pt,
   // fr,
-}
+};
 
 const i18n = createI18n({
-  locale: 'en', 
-  fallbackLocale: 'en', 
+  locale: "pt-BR",
+  fallbackLocale: "en",
   messages,
   legacy: false,
   globalInjection: true, // This enables $t in templates
-})
+});
 
-export default i18n
+export default i18n;
